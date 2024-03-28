0f86d0ddf084f33b07b2df5938b85dd94984acb5
acikgozb <berkacikgoz@armut.com>
2024-03-28 20:20:34 +0300

    [exec-tempgen] Remove log file
    Since the logger is quite simple, I decided to keep the plugin simple and have a single entrypoint.
    Therefore, this file is removed and the logger is added to init.lua

--- lua/tempgen/init.lua
+++ lua/tempgen/init.lua
@@ -1,14 +1,31 @@
 local M = {}
 
-local log = require("tempgen.log")
+local log = require("plenary.log").new({
+	plugin = "tempgen",
+	level = "info",
+})
+
+local telescope_builtin = require("telescope.builtin")
+local telescope_actions = require("telescope.actions")
+local telescope_actions_state = require("telescope.actions.state")
+local telescope_utils = require("telescope.utils")
+local config = {}
+
+local function setTemplatePath(path)
+	config.path = path
+end
 
 function M.setup(opts)
 	if opts == nil or opts.path == nil then
-		log.warn("User path for templates are not specified in config. Therefore, the plugin setup is skipped.") -- log warning to user.
+		log.warn("User path for templates are not specified in config. Therefore, the plugin setup is skipped.")
 		return
 	end
 
-	print(opts.path)
+	setTemplatePath(opts.path)
 end
+
+vim.api.nvim_create_user_command("Tempgen", function()
+	print("exec tempgen")
+end, {})
 
 return M
